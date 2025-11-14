2f69b67db7ba662e98bbc73fa6cc7102be519d89
Anderson Valencia <avalenciacs@gmail.com>
2025-11-14 17:24:49 +0100

    Solved lab

--- src/books.js
+++ src/books.js
@@ -38,21 +38,86 @@
 
 
 // Your code here:
-const booksArray = [];
+const booksArray = [
 
+  {
+    title: "The Old Man and the Sea",
+    pages: 128,
+    author: "Ernest Hemingway",
+    details: {
+      language: "English",
+      description: "One of Hemingway's most famous works, it tells the story of Santiago..."
+    }
+  },
+  {
+    title: "The Airbnb Story",
+    pages: 256,
+    author: "Leight Gallagher",
+    details: {
+      language: "English",
+      description: "This is the remarkable behind-the-scenes story of the creation and growth of Airbnb..."
+    }
+  },
+  {
+    title: "Educated - A Memoir",
+    pages: 352,
+    author: "Tara Westover",
+    details: {
+      language: "English",
+      description: "Educated is an account of the struggle for self-invention..."
+    }
+  },
+  {
+    title: "The Art of Learning",
+    pages: 288,
+    author: "Josh Waitzkin",
+    details: {
+      language: "English",
+      description: "The Art of Learning takes readers through Waitzkin's unique journey to excellence. He explains in clear detail how a well-thought-out, principled approach to learning is what separates success from failure."
+    }
+  },
+];
 
+console.log(booksArray[0]);
+console.log(booksArray[1]);
+console.log(booksArray[2]);
+console.log(booksArray[3]);
+
+booksArray.forEach(libro => {
+  console.log('---------libro---------');
+  console.log(`Título: ${libro.title}`);
+  console.log(`Páginas: ${libro.pages}`);
+  console.log(`Autor: ${libro.author}`);
+  console.log('---------details---------');
+  console.log(`Idioma: ${libro.details.language}`);
+  console.log(`Descripción: ${libro.details.description}`);
+});
+
+for (const libro of booksArray) {
+  console.log("-----------libro-----------");
+  console.log(libro.title);
+  console.log(libro.pages);
+  console.log(libro.author);
+   console.log("---------details----------");
+  console.log(libro.details.language);
+    console.log(libro.details.description);
+}
 
 
 // Iteration 2 | Book Details
-function getBookDetails() {
+function getBookDetails(libro) {
   // Your code here:
-
+return `${libro.title} - ${libro.author} - ${libro.pages} pages`;
 }
+console.log(getBookDetails(booksArray[1]));
 
 
 
 // Iteration 3 | Delete Language
 // Your code here:
+for (const libro of booksArray) {
+    delete libro.details.language;
+}
 
 
 
@@ -60,7 +125,11 @@ function getBookDetails() {
 // Iteration 4 | Estimated Reading Time
 // Your code here:
 
-
+for (const libro of booksArray){
+  const readingTime=Math.ceil(libro.pages*500/90)
+  libro.readingTime = readingTime;
+}
+console.log(booksArray[0]);
 
 
 // Bonus: Iteration 5 | Books Dictionary
